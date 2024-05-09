3f7cf70bca6758bf5b129d8860cbd9b17689d2f2
Nayane de Souza <159593131+nayanesenhorinha@users.noreply.github.com>
2024-05-09 20:10:30 -0300

    Update adicionar_estudante_pd.sql

--- adicionar_estudante_pd.sql
+++ adicionar_estudante_pd.sql
@@ -20,17 +20,14 @@ DECLARE
     _endereco_id INT;
     _pessoa_id INT;
 BEGIN
-    -- Inserir endereço na tabela Endereco
     INSERT INTO escola.Endereco (Rua, Numero, Bairro, Cidade, Estado, Pais, Cep)
     VALUES (_rua, _numeroEndereco, _bairro, _cidade, _estado, _pais, _cep)
     RETURNING ID_Endereco INTO _endereco_id;
 
-    -- Inserir pessoa na tabela Pessoa
     INSERT INTO escola.Pessoa (NomeCompleto, DataNascimento, Genero, Email, Telefone, ID_Endereco)
     VALUES (_nomeCompleto, _dataNascimento, _genero, _email, _telefone, _endereco_id)
     RETURNING ID_Pessoa INTO _pessoa_id;
 
-    -- Inserir estudante na tabela Estudante
     INSERT INTO escola.Estudante (NumeroMatricula, DataMatricula, Status, ID_Pessoa)
     VALUES (_numeroMatricula, _dataMatricula, _status, _pessoa_id);
 END;
